2c09004d646409d2826bf6be1e0e8023528a4037
Neelesh Chandwani <neeleshchandwani@gmail.com>
2022-01-11 12:50:23 +0530

    Display products on frontend & installed material UI

--- backend/models/products.js
+++ backend/models/products.js
@@ -15,6 +15,7 @@ const productSchema = new mongoose.Schema({
     },
     quantity: {
         type: Number,
+        required: true,
         default: 1,
     },
     imgUrl: {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import Nav from './Nav';
+import './App.css';
+
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -7,10 +10,31 @@ function App() {
     const url = "/api/products";
     fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => setProducts(data));
   }, [])
 
-  return <div className="App">HIII</div>;
+  return <div className="App">
+    <Nav />
+    <Home products={products} />
+  </div>;
+}
+
+const Home = ({ products }) => {
+  return <div className="products-catalog">
+    {products.map((item) => {
+      return <div className="product" key={item._id}>
+        <div className="product-top">
+          <img src={item.imgUrl} />
+        </div>
+        <div className="product-bottom">
+          <h2>{item.name}</h2>
+          <span>{item.description}</span>
+          <span>In stock: {item.quantity}</span>
+          <span>Category: {item.category}</span>
+        </div>
+      </div>
+    })}
+  </div>
 }
 
 export default App;
