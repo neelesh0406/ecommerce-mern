5db92fafcd72a266f40b62c9f8c509ba4bfa3da3
Neelesh Chandwani <neeleshchandwani@gmail.com>
2022-01-11 16:22:48 +0530

    Setup react router dom. single product page with useParams() added

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,12 +2,17 @@ const router = require('express').Router();
 const Product = require('../models/products');
 
 
-
+// @route - GET /api/product/
+// @desc  - get all products
+// @access- PUBLIC
 router.get('/', async (req, res) => {
     const products = await Product.find();
     res.json(products);
 })
 
+// @route - POST /api/product/
+// @desc  - add product
+// @access- PUBLIC
 router.post('/', async (req, res) => {
     const obj = {
         name: req.body.name,
@@ -20,6 +25,14 @@ router.post('/', async (req, res) => {
     const newProduct = new Product(obj);
     const response = await newProduct.save();
     res.json(response);
+})
+
+// @route - get /api/product/:id
+// @desc  - get single product
+// @access- PUBLIC
+router.get('/:id', async (req, res) => {
+    const singleProduct = await Product.findById(req.params.id);
+    res.json(singleProduct);
 })
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Nav from './Nav';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes, useParams } from "react-router-dom";
 
 
 function App() {
@@ -13,28 +14,65 @@ function App() {
       .then(data => setProducts(data));
   }, [])
 
-  return <div className="App">
-    <Nav />
-    <Home products={products} />
-  </div>;
+  return <BrowserRouter>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home products={products} />} />
+        <Route path='/products/:id' element={<SingleProduct />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+    ;
 }
 
 const Home = ({ products }) => {
   return <div className="products-catalog">
     {products.map((item) => {
       return <div className="product" key={item._id}>
-        <div className="product-top">
-          <img src={item.imgUrl} />
-        </div>
-        <div className="product-bottom">
-          <h2>{item.name}</h2>
-          <span>{item.description}</span>
-          <span>In stock: {item.quantity}</span>
-          <span>Category: {item.category}</span>
-        </div>
+        <Link to={`/products/${item._id}`}>
+          <div className="product-top">
+            <img src={item.imgUrl} />
+          </div>
+          <div className="product-bottom">
+            <h2>{item.name}</h2>
+            <span>{item.description}</span>
+            <span>In stock: {item.quantity}</span>
+            <span>Category: {item.category}</span>
+          </div>
+        </Link>
       </div>
     })}
   </div>
+}
+
+const SingleProduct = () => {
+  const [singleProduct, setSingleProduct] = useState({});
+  const id = useParams().id;
+
+  useEffect(() => {
+    const url = `/api/products/${id}`
+    const getSingleProduct = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setSingleProduct(data);
+    }
+    getSingleProduct();
+  }, [])
+
+  const { name, description, quantity, category, imgUrl } = singleProduct;
+
+  return <>
+    <div className="single-product-top">
+      <img src={imgUrl} />
+    </div>
+    <div className="single-product-bottom">
+      <h2>{name}</h2>
+      <span>{description}</span>
+      <span>In stock: {quantity}</span>
+      <span>Category: {category}</span>
+    </div>
+  </>
 }
 
 export default App;
